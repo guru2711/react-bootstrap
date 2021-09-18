12a025c5ed139b24ea2087b860a23052c6447ded
guru2711 <59472229+guru2711@users.noreply.github.com>
2021-09-18 18:31:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Plus } from "./Plus";
+import Free from "./Free";
+import { Pro } from "./Pro";
+function App() {
+  return (
+    <div >
+      <h1>React-</h1>
+      <Free />
+      <Plus />
+      <Pro />
+    </div>
+  );
+}
+
+export default App;
